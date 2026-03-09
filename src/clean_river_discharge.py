6be5c584644843f899a74b8afb152e091fc86db5
H-Sto <hstobwasser@gmail.com>
2026-03-09 11:37:22 +0000

    Streamlined clean_river)discharge.py flow to use predefined function

--- src/clean_river_discharge.py
+++ src/clean_river_discharge.py
@@ -34,25 +34,4 @@ def main() -> None:
    print(f"Rows: {len(df_clean)}")
 
 if __name__ == "__main__":
-   df = pd.read_csv(RAW_PATH)
-
-   print("Raw data preview:")
-   print(df)
-
-   print("\nRaw data info") 
-   print(df.info())
-
-   df["Mean_Discharge_m3s"] = pd.to_numeric(
-      df["Mean_Discharge_m3s"], errors="coerce"
-   )
-
-print("\nAfter converting discharge to numeric:")
-print(df)
-
-df_clean = df.dropna(subset=["Mean_Discharge_m3s"])
-
-print("\nAfter dropping rows with missing discharge values:")
-print(df_clean)
-
-df_clean.to_csv(PROCESSED_PATH, index=False)
-print(f"\nCleaned data saved to {PROCESSED_PATH}")
+   main()
